a7c86ddc7569f56c877b7a96456c02ccec84eac6
Cavallin021 <joaovictorcavallin07@gmail.com>
2026-05-07 00:44:10 -0300

    fix: NUNCA MAIS USAR PRISMA 7

--- src/usuarios/usuarios.service.ts
+++ src/usuarios/usuarios.service.ts
@@ -1,11 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateUsuarioDto } from './dto/create-usuario.dto';
 import { UpdateUsuarioDto } from './dto/update-usuario.dto';
+import * as bcrypt from 'bcrypt';
+import { PrismaService } from '../../prisma/prisma.service';
 
 @Injectable()
 export class UsuariosService {
-  create(createUsuarioDto: CreateUsuarioDto) {
-    return 'This action adds a new usuario';
+  constructor(private prisma: PrismaService) {}
+
+  async create(createUsuarioDto: CreateUsuarioDto) {
+    const { email, senha_hash, ...userInfo } = createUsuarioDto;
+
+    const userExist = await this.prisma.usuario.findUnique({
+      where: { email },
+    });
+
+    if (userExist) throw new BadRequestException('O email já esta cadastrado');
+
+    const hashed_password = await bcrypt.hash(senha_hash, 10);
+
+    return this.prisma.usuario.create({
+      data: { ...userInfo, email, senha_hash: hashed_password },
+      select: { createdAt: true },
+    });
   }
 
   findAll() {
