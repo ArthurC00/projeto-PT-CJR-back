54273ad1038aa5a312c0219c740596d7255e7899
Arthur <aoscunhaa@gmail.com>
2026-05-10 01:13:30 -0300

    feat: CRUD das lojas

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsuariosModule } from './usuarios/usuarios.module';
 import { AuthModule } from './auth/auth.module';
+import { LojasModule } from './lojas/lojas.module';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Module({
-  imports: [UsuariosModule, AuthModule],
+  imports: [UsuariosModule, AuthModule, LojasModule ], 
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
