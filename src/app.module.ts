c6ae91de81a6b378f23a632ce5dc122c4d16b068
Ana Luisa <camara.analui@gmail.com>
2026-05-12 13:11:44 -0300

    feat: esqueleto do CRUD de categorias

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsuariosModule } from './usuarios/usuarios.module';
 import { AuthModule } from './auth/auth.module';
+import { CategoriasModule } from './categorias/categorias.module';
 
 @Module({
-  imports: [UsuariosModule, AuthModule],
+  imports: [UsuariosModule, AuthModule, CategoriasModule],
   controllers: [AppController],
   providers: [AppService],
 })
