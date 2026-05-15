6b6a2ce94990da50778dca9f24bb21c06bdd3dbf
Cavallin021 <joaovictorcavallin07@gmail.com>
2026-05-15 14:24:41 -0300

    fix: ta funcionando o crud

--- src/produto/produto.service.ts
+++ src/produto/produto.service.ts
@@ -3,52 +3,50 @@ import { CreateProdutoDto } from './dto/create-produto.dto';
 import { UpdateProdutoDto } from './dto/update-produto.dto';
 import { PrismaService } from '../../prisma/prisma.service';
 
-
 @Injectable()
 export class ProdutoService {
-  constructor (private readonly prisma: PrismaService){}
+  constructor(private readonly prisma: PrismaService) {}
 
   async create(createProdutoDto: CreateProdutoDto) {
-  return await this.prisma.produtos.create({
-    data: {
-      ...createProdutoDto 
-    }
-  });
-}
+    console.log(createProdutoDto);
+    return await this.prisma.produtos.create({
+      data: {
+        ...createProdutoDto,
+      },
+    });
+  }
 
   async findAll() {
-    const produto = await this.prisma.produtos.findMany
+    const produto = await this.prisma.produtos.findMany();
     return produto;
-  
   }
 
   async findOne(id: number) {
     const produto = await this.prisma.produtos.findUnique({
-      where:{id}
-    })
+      where: { id },
+    });
     return produto;
-
   }
 
   async update(id: number, updateProdutoDto: UpdateProdutoDto) {
     const produto = this.findOne(id);
-    if (!produto){ 
-      throw new Error;
+    if (!produto) {
+      throw new Error();
     }
-    return await this.prisma.produtos.update ({
-      where:{id},
-      data:{...updateProdutoDto}
+    return await this.prisma.produtos.update({
+      where: { id },
+      data: { ...updateProdutoDto },
     });
   }
 
   async remove(id: number) {
     const produto = this.findOne(id);
-    if (!produto){ 
-      throw new Error;
+    if (!produto) {
+      throw new Error();
     }
 
-    return await this.prisma.produtos.delete ({
-      where:{id},
+    return await this.prisma.produtos.delete({
+      where: { id },
     });
   }
 }
