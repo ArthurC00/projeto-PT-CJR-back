5648a02bf025d8f61a2a5b867e61bf29e1d14c55
Juju Luz <julialuz@MacBook-Air-de-Julia.local>
2026-05-21 19:13:06 -0300

    feat: crud de avaliacoes_loja

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { UsuariosModule } from './usuarios/usuarios.module';
 import { AuthModule } from './auth/auth.module';
 import { ProdutoModule } from './produto/produto.module';
 import { AvaliacoesProdutoModule } from './avaliacoes_produto/avaliacoes_produto.module';
+import { AvaliacoesLojaModule } from './avaliacoes_loja/avaliacoes_loja.module';
 
 @Module({
-  imports: [UsuariosModule, AuthModule, ProdutoModule, AvaliacoesProdutoModule],
+  imports: [UsuariosModule, AuthModule, ProdutoModule, AvaliacoesProdutoModule, AvaliacoesLojaModule],
   controllers: [AppController],
   providers: [AppService],
 })
