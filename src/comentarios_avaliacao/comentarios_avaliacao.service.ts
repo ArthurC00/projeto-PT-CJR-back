7d80c300bfa01c33706e4b19a3961748ee6d1c18
Ana Luisa <camara.analui@gmail.com>
2026-06-02 13:42:01 -0300

    feat: função create (testar)

--- src/comentarios_avaliacao/comentarios_avaliacao.service.ts
+++ src/comentarios_avaliacao/comentarios_avaliacao.service.ts
@@ -1,11 +1,47 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateComentariosAvaliacaoDto } from './dto/create-comentarios_avaliacao.dto';
 import { UpdateComentariosAvaliacaoDto } from './dto/update-comentarios_avaliacao.dto';
+import { PrismaService } from '../../prisma/prisma.service';
 
 @Injectable()
 export class ComentariosAvaliacaoService {
-  create(createComentariosAvaliacaoDto: CreateComentariosAvaliacaoDto) {
-    return 'This action adds a new comentariosAvaliacao';
+  constructor(private readonly prisma:PrismaService){}
+  
+  async create(createComentariosAvaliacaoDto: CreateComentariosAvaliacaoDto) {
+    const { conteudo, usuario_id, avaliacao_loja_id, avaliacao_produto_id } = createComentariosAvaliacaoDto;
+
+    // dois caminhos: comentário numa avaliação de produto OU comentário numa avaliação de loja
+
+    if (avaliacao_loja_id) {
+      // se tiver recebido o ID de uma avaliação de loja
+      const avaliacaoExiste = await this.prisma.avaliacoes_loja.findUnique({
+        where: { id: avaliacao_loja_id }
+      });
+
+      if(!avaliacaoExiste) {
+        throw new BadRequestException("A avaliação de loja especificada não existe.")
+      }
+      return this.prisma.comentarios_avaliacao.create({
+        data: { conteudo, usuario_id, avaliacao_loja_id },
+        select: { id: true, usuario_id: true, avaliacao_loja_id: true, conteudo: true }
+      });
+    } else if (avaliacao_produto_id) {
+      // se tiver recebido o ID de uma avaliação de produto
+      const avaliacaoExiste = await this.prisma.avaliacoes_produto.findUnique({
+        where: { id: avaliacao_produto_id }
+      });
+
+      if(!avaliacaoExiste) {
+        throw new BadRequestException("A avaliação de produto especificada não existe.")
+      }
+
+      return this.prisma.comentarios_avaliacao.create({
+        data: { conteudo, usuario_id, avaliacao_produto_id },
+        select: { id: true, usuario_id: true, avaliacao_produto_id: true, conteudo: true }
+      });
+    } else {
+      throw new BadRequestException("ID de avaliação não recebido.")
+    }
   }
 
   findAll() {
