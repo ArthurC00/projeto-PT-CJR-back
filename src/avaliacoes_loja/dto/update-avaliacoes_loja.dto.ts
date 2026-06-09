50db836b68d63f941729e1456a84a17f7f447a03
João Victor Cavallin <135937000+Cavallin021@users.noreply.github.com>
2026-06-09 12:17:52 -0300

    Revert "Conserto de erros de digitação na autenticação"

--- src/avaliacoes_loja/dto/update-avaliacoes_loja.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateAvaliacoesLojaDto } from './create-avaliacoes_loja.dto';
-
-export class UpdateAvaliacoesLojaDto extends PartialType(CreateAvaliacoesLojaDto) {}
